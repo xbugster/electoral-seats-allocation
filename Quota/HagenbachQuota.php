626659bd9eabac1d41caf0c60da9a458fa55aadb
Leader VR <leaderpvp@gmail.com>
2014-08-06 22:16:37 -0700

    changed algo to hagenbachs

--- Quota/HagenbachQuota.php
+++ Quota/HagenbachQuota.php
@@ -3,11 +3,11 @@
  * Created with PhpStorm.
  * User: Valentin Ruskevych <leaderpvp@gmail.com>
  * Date: 8/6/14
- * Time: 9:57 PM
+ * Time: 10:14 PM
  */
 
-class DroopQuota extends QuotaAbstract {
+class HagenbachQuota extends QuotaAbstract {
     public function _calculateQuota($totalSeats, $totalVotes) {
-        return 1 + ( $totalSeats / ( 1 + $totalSeats ) );
+        return $totalSeats / ( 1 + $totalSeats );
     }
 }
