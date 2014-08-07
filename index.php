9535e642d59fb00b11ae94c123e31d307d7bf14e
Valentin Ruskevych <valentinr@netvertise.co.il>
2014-08-07 15:47:21 +0300

    Added example with all 4 quotas to index.php.

--- index.php
+++ index.php
@@ -20,10 +20,41 @@ $autoloader->register();
 
 $electoralData = array("A" => 15000, "B" => 5400, "C" => 5500, "D" => 5550);
 $totalSeats = 15;
+
+# Hare Quota Example
 $calculator = new ElectoralSeatsAllocator(new Quota\HareQuota, $totalSeats, $electoralData);
-$seats = $calculator->allocate();
+$hareResult = $calculator->allocate();
 
+$calculator = new ElectoralSeatsAllocator(new Quota\DroopQuota, $totalSeats, $electoralData);
+$droopResult = $calculator->allocate();
 
+$calculator = new ElectoralSeatsAllocator(new Quota\HagenbachQuota, $totalSeats, $electoralData);
+$hagenbachResult = $calculator->allocate();
+
+$calculator = new ElectoralSeatsAllocator(new Quota\ImperialQuota, $totalSeats, $electoralData);
+$imperialResult = $calculator->allocate();
+
+echo 'Elections Results Based on data sent:' . PHP_EOL;
+echo 'Total seats: ' . $totalSeats . PHP_EOL;
+echo 'Elections Result: ' . PHP_EOL;
+var_dump($electoralData);
+
+echo '<h4>Hare Quota Example Result</h4>';
 echo '<pre>';
-var_dump($seats);
+var_dump($hareResult);
+echo '</pre>';
+
+echo '<h4>Droop Quota Example Result</h4>';
+echo '<pre>';
+var_dump($droopResult);
+echo '</pre>';
+
+echo '<h4>Hagenbach Quota Example Result</h4>';
+echo '<pre>';
+var_dump($hagenbachResult);
+echo '</pre>';
+
+echo '<h4>Imperial Quota Example Result</h4>';
+echo '<pre>';
+var_dump($imperialResult);
 echo '</pre>';
