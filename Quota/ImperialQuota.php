6913d821900248dc2a759339affc95996950be9b
Leader VR <leaderpvp@gmail.com>
2014-08-06 22:19:38 -0700

    added imperial quota

---
+++ Quota/ImperialQuota.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created with PhpStorm.
+ * User: Valentin Ruskevych <leaderpvp@gmail.com>
+ * Date: 8/6/14
+ * Time: 10:17 PM
+ */
+
+class ImperialQuota extends QuotaAbstract {
+    public function _calculateQuota($totalSeats, $totalVotes) {
+        return $totalSeats / ( 2 + $totalSeats );
+    }
+}
